38a1f3037ba46b1a88da816bb6066738db5fbb62
蒋世伟 <owen.jsw@icloud.com>
2017-07-31 21:19:33 +0800

    strlower

--- server/application/controllers/Index.php
+++ server/application/controllers/Index.php
@@ -6,7 +6,7 @@ class Index extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->database();
-		$this->load->library('baidumapapi_util');
+		$this->load->library('BaiduMapApi_util');
 		$this->load->library('nick_util');
 		$this->load->model('relation_model');
 	}
